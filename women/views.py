a656d139d3220d71797cf32dcb67dd83bccaefbf
Ahmedik <isa732827@gmail.com>
2025-05-26 01:32:36 +0300

    Создал сериализатор

--- women/views.py
+++ women/views.py
@@ -2,9 +2,10 @@ from rest_framework import generics
 from django.shortcuts import render
 
 from models import Women
+from serializers import WomenSerializer
 # Create your views here.
 
 
 class WomenApiView(generics):
     queryset = Women.objects.all()
-    serializer_class = W
+    serializer_class = WomenSerializer
