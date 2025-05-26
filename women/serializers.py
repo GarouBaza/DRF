a3d55b93c0338c059658de9374694a5f98a39cbb
Ahmedik <isa732827@gmail.com>
2025-05-27 00:25:22 +0300

    Добавил проверку что бы нельзя было дабавлять существующие посты

--- women/serializers.py
+++ women/serializers.py
@@ -8,30 +8,35 @@ from women.models import Women
 
 
 
-class WomenModel:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
-
-
-
 class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     content = serializers.CharField(max_length=255)
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=False)
+    cat_id = serializers.IntegerField()
 
 
-def encode():
-    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
-    models_sr = WomenSerializer(model)
-    print(models_sr.data, type(models_sr.data), sep='\n')
-    json = JSONRenderer().render(models_sr.data)
-    print(json)
 
 
-def decode():
-    json_bytes = b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}'
-    stream = io.BytesIO(json_bytes)
-    data = JSONParser().parse(stream)
-    serializer = WomenSerializer(data=data)
-    if serializer.is_valid():
-        print(serializer.validated_data)
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+# def encode():
+#     model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+#     models_sr = WomenSerializer(model)
+#     print(models_sr.data, type(models_sr.data), sep='\n')
+#     json = JSONRenderer().render(models_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     json_bytes = b'{"title": "Angelina Jolie", "content": "Content: Angelina Jolie"}'
+#     stream = io.BytesIO(json_bytes)
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     if serializer.is_valid():
+#         print(serializer.validated_data)
--- women/views.py
+++ women/views.py
@@ -8,16 +8,29 @@ from .serializers import WomenSerializer  # или from women.serializers
 
 class WomenApiView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        if Women.objects.filter(
+                title=request.data['title'],
+                content=request.data['content'],
+                cat_id=request.data['cat_id']
+        ).exists():
+            return Response(
+                {"error": "Такой пост уже существует."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id'],
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 
     def delete(self,request ,id):
         delete_object = Women.objects.filter(id=id).delete()
